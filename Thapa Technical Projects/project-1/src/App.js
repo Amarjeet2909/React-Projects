851fab9a62ad081b3c2a6b9e659d6ac20d344f53
Amarjeet2909 <ak4348225@gmail.com>
2023-06-30 23:11:15 +0530

    Restaurant project in react

---
+++ Thapa Technical Projects/project-1/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Resturant from "./component/Basics/Resturant";
+import "./App.css"
+
+const App = () => {
+  return <>
+  <h1 className='header'>Welcome to RecruitNXT Restaurant</h1>
+  <Resturant />
+  </>
+}
+
+export default App

d765fe9d928846ee4b0dc9d7b6f4bb751b4ceafe
Amarjeet2909 <ak4348225@gmail.com>
2023-07-03 18:14:26 +0530

    Project 2 (Use of Hooks in react)

--- Thapa Technical Projects/project-1/src/App.js
+++ Thapa Technical Projects/project-1/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import Resturant from "./component/Basics/Resturant";
-import "./App.css"
+import React from "react";
+
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+
+import UseReducer from "./component/Hooks/useReducer";
 
 const App = () => {
   return <>
-  <h1 className='header'>Welcome to RecruitNXT Restaurant</h1>
-  <Resturant />
+  <h1 className='header'>Welcome to RecruitNXT</h1>
+  <UseReducer />
   </>
-}
+};
 
-export default App
+export default App;
